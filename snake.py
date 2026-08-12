2b924bbbca2b4ce995318042a85b2e354858b7a9
valenbasiuk <valenbasiukcls@gmail.com>
2026-08-12 16:22:39 -0300

    snake.py negro a blanco

--- snake.py
+++ snake.py
@@ -102,7 +102,7 @@ def run_game():
         else:
             snake.pop()
 
-        pantalla.fill(NEGRO)
+        pantalla.fill(VERDE)
         for pos in snake:
             pygame.draw.rect(pantalla, VERDE, (*pos, TAM_BLOQUE, TAM_BLOQUE))
         pygame.draw.rect(pantalla, ROJO, (*comida, TAM_BLOQUE, TAM_BLOQUE))
